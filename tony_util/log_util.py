f16ad4d3096372e716227c2173436a515578c741
Tony Held <tony.held@gmail.com>
2020-12-26 17:20:21 -0800

    Logger utility functions added

--- tony_util/log_util.py
+++ tony_util/log_util.py
@@ -2,44 +2,62 @@
 Routines and classes to assist in logging.
 Created by: Tony Held tony.held@gmail.com
 Created on: 2020-12-26
-Copyright © 2020 Tony Held.  All rights reserved."""
+Copyright © 2020 Tony Held.  All rights reserved.
+
+Notes
+______
+
+Level Name, Value,  Use
+----------  -----   --------------------------------------------------------------------------
+NOTSET      0       When the logger is created with getLogger(), the level is set to NOTSET.
+                    If the logging level is not set explicitly with setLevel(),
+                    the messages are propagated to the logger parents.
+DEBUG       10      Detailed information, typically of interest only when diagnosing problems.
+INFO        20      Confirmation that things are working as expected.
+WARNING     30      An indication that something unexpected happened,
+                    or indicative of some problem in the near future (e.g. ‘disk space low’).
+                    The software is still working as expected.
+ERROR       40      Due to a more serious problem,
+                    the software has not been able to perform some function.
+CRITICAL    50      A serious error,
+                    indicating that the program itself may be unable to continue running.
+                    """
 
 import logging
 import sys
 import os
 
+# todo - double check new filters
 
-class FilterDebugInfoNotset(logging.Filter):
-    """Allow only DEBUG, INFO, & NOTSET records to be logged."""
+class FilterOutWarningsErrors(logging.Filter):
+    """Filter out logging.WARNING or greater messages.
+    Primary use of this class is filter records sent to stdout so they don't
+    duplicate records that will also be sent to stderr."""
     def filter(self, record):
         # print(f'{record} \n{record.levelno} \n {record.levelname}')
-        if record.levelno in [logging.DEBUG, logging.INFO, logging.NOTSET]:
-            return True
-        else:
+        if record.levelno >= logging.WARNING:
             return False
-
-
-class FilterInfo(logging.Filter):
-    """Allow only INFO records to be logged."""
-    def filter(self, record):
-        # print(f'{record} \n{record.levelno} \n {record.levelname}')
-        if record.levelno in [logging.INFO]:
-            return True
         else:
-            return False
+            return True
 
 
 class SingletonLogger:
     """Application level logger with the following characteristics:
-        1) Logger has a name and the logger with the same name is shared application-wide
-        2) DEBUG or greater logs go to log file: "logs/<logger_name>.log"
+        1) Loggers with the same name will be shared application-wide
+        2) Loggers will log to a file and to stdout and stderr
         3) WARNING or greater logs go to sys.stderr
-        4) INFO only logs go to sys.stdout
+        4) log level for sys.stdout can be set (default is INFO).
+        3) The file name of the log file is: "<path>/<name>.log"
         """
     loggers = {}
 
     @classmethod
-    def get_logger(cls, name='my_logger', path='logs'):
+    def get_logger(cls,
+                   name='my_logger',
+                   path='logs',
+                   stdout_level=logging.INFO,
+                   file_level=logging.DEBUG,
+                   ):
         """Get logger if it exists, otherwise create it..
 
             Parameters
@@ -50,32 +68,43 @@ class SingletonLogger:
             path : str
                 path for logger output file
 
+            stdout_level: logging.DEBUG | logging.INFO
+                logging level for stdout output.
+
+            file_level: int
+                logging level for output file.
+
             Returns
             -------
-            logger : type***
+            logger : logging
                 Application wide logger named 'name'
             """
         if name not in SingletonLogger.loggers:
-            SingletonLogger.create_logger(name, path)
+            SingletonLogger.create_logger(name, path, stdout_level, file_level)
         return SingletonLogger.loggers[name]
 
     @classmethod
-    def create_logger(cls, name='my_logger', path='logs'):
-        """Get create logger named 'name' and add it to the class level dictionary
+    def create_logger(cls, name='my_logger',
+                      path='logs',
+                      stdout_level=logging.INFO,
+                      file_level=logging.DEBUG,
+                      ):
+        """Create logger named 'name' and add it to the class level dictionary
 
-            Parameters
-            ----------
-            name : str
-                Name of logger
+        Parameters
+        ----------
+        name : str
+            Name of logger
 
-            path : str
-                path for logger output file
+        path : str
+            path for logger output file
 
-            Returns
-            -------
-            logger : type***
-                Application wide logger named 'name'
-            """
+        stdout_level: logging.DEBUG | logging.INFO
+            logging level for stdout output.
+
+        file_level: int
+            logging level for output file.
+        """
         # create path if it does not exist yet
         if os.path.isfile(path):
             raise FileExistsError(f'Attempt to create a directory that is already a regular file:\n {path}')
@@ -88,14 +117,14 @@ class SingletonLogger:
         logger.setLevel(logging.DEBUG)
 
         fh = logging.FileHandler(file_name)
-        fh.setLevel(logging.DEBUG)
+        fh.setLevel(file_level)
 
         sh_err = logging.StreamHandler(stream=sys.stderr)
         sh_err.setLevel(logging.WARNING)
 
         sh_out = logging.StreamHandler(stream=sys.stdout)
-        sh_out.setLevel(logging.INFO)
-        sh_out.addFilter(FilterInfo())
+        sh_out.setLevel(stdout_level)
+        sh_out.addFilter(FilterOutWarningsErrors())
 
         logger.addHandler(fh)
         logger.addHandler(sh_err)
@@ -106,14 +135,16 @@ class SingletonLogger:
 
 if __name__ == "__main__":
 
-    log = SingletonLogger.get_logger()
-    log = SingletonLogger.get_logger('bob')
-    log = SingletonLogger.get_logger('alice')
+    log1 = SingletonLogger.get_logger()
+    log2 = SingletonLogger.get_logger('bob')
+    log3 = SingletonLogger.get_logger('alice')
     print(SingletonLogger.loggers)
 
-    print(type(log), log)
-    log.debug(f'Called with debug 10')
-    log.info(f'Called with info 20')
-    log.warning(f'Called with warning 30')
-    log.error(f'Called with error 40')
-    log.critical(f'Called with critical 50')
+    print(type(log1), log1)
+    print(type(log2), log2)
+    print(type(log3), log3)
+    log3.debug(f'Called with debug 10')
+    log3.info(f'Called with info 20')
+    log3.warning(f'Called with warning 30')
+    log3.error(f'Called with error 40')
+    log3.critical(f'Called with critical 50')
